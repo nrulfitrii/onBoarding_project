c11d8ca64dd7c2c35e6bad1788a31318cffd0abd
nrulfitrii <137499178+nrulfitrii@users.noreply.github.com>
2023-11-11 15:29:03 +0700

    Add files via upload

---
+++ intro_screen.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'intro_widget.dart';
+
+class AppScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
+}
+
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({super.key});
+
+  @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  final PageController _pageController = PageController();
+
+  int _activePage = 0;
+
+  void onNextPage() {
+    if (_activePage < _pages.length - 1) {
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.fastEaseInToSlowEaseOut,
+      );
+    }
+  }
+
+  final List<Map<String, dynamic>> _pages = [
+    {
+      'color': '#ffe24e',
+      'title': 'Jadwal Penyewaan Fleksibel',
+      'image': 'assets/images/clock.png',
+      'description':
+          "Atur jadwal penyewaan kamera Anda sesuai kebutuhan Anda. Fleksibilitas adalah kunci untuk memastikan Anda mendapatkan kamera saat Anda membutuhkannya.",
+      'skip': true
+    },
+    {
+      'color': '#a3e4f1',
+      'title': 'Pembayaran Aman dan Mudah',
+      'image': 'assets/images/payment.png',
+      'description':
+          'Lakukan pembayaran dengan aman menggunakan berbagai metode pembayaran. Kami menjamin transaksi yang lancar dan aman bagi Anda.',
+      'skip': true
+    },
+    {
+      'color': '#31b77a',
+      'title': 'Dukungan Pelanggan 24/7',
+      'image': 'assets/images/cs.png',
+      'description':
+          'Tim dukungan pelanggan kami siap membantu Anda kapan saja. Pertanyaan atau masalah? Kami di sini untuk membantu!',
+      'skip': false
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView.builder(
+              controller: _pageController,
+              itemCount: _pages.length,
+              scrollBehavior: AppScrollBehavior(),
+              onPageChanged: (int page) {
+                setState(() {
+                  _activePage = page;
+                });
+              },
+              itemBuilder: (BuildContext context, int index) {
+                return IntroWidget(
+                  index: index,
+                  color: _pages[index]['color'],
+                  title: _pages[index]['title'],
+                  description: _pages[index]['description'],
+                  image: _pages[index]['image'],
+                  skip: _pages[index]['skip'],
+                  onTab: onNextPage,
+                );
+              }),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 1.75,
+            right: 0,
+            left: 0,
+            child: Column(
+              children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: _buildIndicator())
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildIndicator() {
+    final indicators = <Widget>[];
+
+    for (var i = 0; i < _pages.length; i++) {
+      if (_activePage == i) {
+        indicators.add(_indicatorsTrue());
+      } else {
+        indicators.add(_indicatorsFalse());
+      }
+    }
+    return indicators;
+  }
+
+  Widget _indicatorsTrue() {
+    final String color;
+    if (_activePage == 0) {
+      color = '#ffe24e';
+    } else if (_activePage == 1) {
+      color = '#a3e4f1';
+    } else {
+      color = '#31b77a';
+    }
+
+    return AnimatedContainer(
+      duration: const Duration(microseconds: 300),
+      height: 6,
+      width: 42,
+      margin: const EdgeInsets.only(right: 8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: hexToColor(color),
+      ),
+    );
+  }
+
+  Widget _indicatorsFalse() {
+    return AnimatedContainer(
+      duration: const Duration(microseconds: 300),
+      height: 8,
+      width: 8,
+      margin: const EdgeInsets.only(right: 8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: Colors.grey.shade100,
+      ),
+    );
+  }
+}
